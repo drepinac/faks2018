272ec8dfe31d3337d388394ddef6430b279e4d22
drepinac <darko.repinac@udu-obz.hr>
2018-05-16 21:27:41 +0200

    Dodana authentikacija

--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,7 @@ Route::get('/admin', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
